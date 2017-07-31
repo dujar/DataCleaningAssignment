3b914d56e45a521b3d8675a2739ff6da35de350a
Fabricio Dujardin <dujar.coding@gmail.com>
2017-07-31 09:18:44 +0200

    further advacements

--- run_analysis.R
+++ run_analysis.R
@@ -10,3 +10,22 @@
 #Uses descriptive activity names to name the activities in the data set
 #Appropriately labels the data set with descriptive variable names.
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+test <- list.files("./UCI HAR Dataset/test")
+
+test.f <- list.files("./UCI HAR Dataset/test/Inertial Signals")
+test.f
+
+train <- list.files("./UCI HAR Dataset/train")
+train.f <- list.files("./UCI HAR Dataset/train/Inertial Signals")
+train.f
+pathtest <- "./UCI HAR Dataset/test/Inertial Signals"
+pathtrain <- "./UCI HAR Dataset/train/Inertial Signals"
+
+for (i in 1:length(train.f)){
+  assign(train.f[i], data.table(read.table(paste(pathtrain,train.f[i],sep="/"))))
+}
+
+body_acc_x_train.txt
+?read.table
